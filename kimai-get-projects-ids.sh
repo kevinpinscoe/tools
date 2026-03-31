c716b40442dbf2da63e26303bde83adb80eae722
Kevin P. Inscoe <kevin.inscoe@gmail.com>
2026-03-31 13:42:16 -0400

    No longer using kimai

--- kimai-get-projects-ids.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TOKEN="$(head -n 1 "$HOME/.config/kimai/api")"
-BASE='https://kimai.kevininscoe.com'
-
-curl -sS \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Accept: application/json" \
-  "${BASE}/api/projects" | jq -r '.[] | "\(.id)\t\(.number // "-")\t\(.name)"'
